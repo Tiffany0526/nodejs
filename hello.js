a933c851a4bdd78207ccc77da8de442a10f2102b
Tiffany <a050138@gmail.com>
2022-05-11 12:45:58 +0800

    my first commit

---
+++ hello.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const title = 'I am Hello Module';
+
+const sayHello = () => {
+    console.log('Hello!');
+}
+const sayGoodnight = () => {
+    console.log('Good night!');
+}
+
+// module.exports.sayHello = sayHello;
+
+// module.exports = {
+//     sayHello: sayHello,
+//     sayGoodnight: sayGoodnight,
+//     title: title,
+// };
+
+// console.log('dirname', __dirname);
+// console.log('filename', __filename);
+// console.log(Path.join(__dirname, 'index.js'));
+
+
+//JS remarks:
+module.exports = {
+    sayHello,  // = sayHello: sayHello,
+    sayGoodnight,
+    title,
+};
