f36d227988e5399487d215a5d1b92e6ac4dfed3d
Tiffany <a050138@gmail.com>
2022-05-11 12:41:50 +0800

    my first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+//第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
+
+//第二個區塊 第三方區塊(套件)
+
+//第三個區塊 自建模組
+// const hello = require('./hello');
+
+///////////////////////////////////////////
+
+// hello.sayHellow();
+// console.log(Hello.title);
