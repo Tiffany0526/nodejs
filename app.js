6b5a4e1482b59a8e4270a1faf4c92676b2adad22
Tiffany <a050138@gmail.com>
2022-05-12 09:10:01 +0800

    app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // 第一個區塊 內建模組
+const path = require('path');
 // const http = require('http');
-const express = require('express')
-// 第二個區塊 第三方模組(套件)
 
+// 第二個區塊 第三方模組(套件)
+const express = require('express')
 // 第三個區塊 自建模組
 
 ////////////////////////////////////////////////////////////////
@@ -39,11 +40,13 @@ app.get((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<head><meta charset="utf-8" /></head>')
-    res.write('<body>')
-    res.write('<h1>這是首頁</h1>')
-    res.write('</body>')
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<head><meta charset="utf-8" /></head>')
+    // res.write('<body>')
+    // res.write('<h1>這是首頁</h1>')
+    // res.write('</body>')
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(3000, () => {
